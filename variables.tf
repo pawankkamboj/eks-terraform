69c62af025a54441fbabe76dbcac42c5b7c86ed6
Pawan Kumar <pawan.kumar@ixigo.com>
2020-05-21 10:42:45 +0530

    remove subnet var

--- variables.tf
+++ variables.tf
@@ -11,11 +11,6 @@ variable "eks_vpc_cidr" {
   default  = "10.0.0.0/16"
 }
 
-variable "subnet_cidrs" {
-  type = list(string)
-  default = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
-}
-
 variable "azs" {
   type = list(string)
   default = ["ap-south-1a", "ap-south-1b", "ap-south-1c"]
